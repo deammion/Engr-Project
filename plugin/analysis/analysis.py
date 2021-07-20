f905c094b257461e0b32b56d5be87ecb2c34668b
James Houlihan <houlihjame@ecs.vuw.ac.nz>
2021-07-20 01:10:52 +0000

    Update analysis.py
    Can successfully filter out and gather all script tags

--- plugin/analysis/analysis.py
+++ plugin/analysis/analysis.py
@@ -1,36 +1,30 @@
 # This is a sample Python script.
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import os
 
 class Analysis:
 
-    def __init__(self, path, html, data_path, scripts):
-        self.path = path
+    def __init__(self, html):
+        # self.path = path
         self.html = html
-        self.data_path = data_path
+        # self.data_path = data_path
         self.scripts = []
+        self.get_tags()
 
-
-def read_response():
-    response = open(self.html)
-    script = None
-
-    for line in response:
-        line.strip
-        if line.startswith("<script"):
-            script = line
-        if script is not None and !line.startswith("</script>"):
-            script += line
-        if line.startswith("</script>"):
-            script += line
-            self.scripts.append(script)
-            script = None
+    def get_tags(self):
+        response = open(self.html)
+        text = response.read()
+        text = text.replace('\n', '')
+        text = text.replace('\t', '')
+        scripts = re.findall('(<script.+?</script>)', text.strip())
+        if scripts:
+            self.scripts = scripts
+            for x in self.scripts:
+                print(x)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    
+    test = Analysis("samples/samples/dev.unshielded.red/1-response.txt")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
