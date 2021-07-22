16acdc055180f09ce9bebf3c3097f2cda315d30f
houlilhjame <houlihjame@ecs.vuw.ac.nz>
2021-07-22 21:50:40 +1200

    Update analysis.py
    Analysis can now read all files in a given directory

--- plugin/analysis/analysis.py
+++ plugin/analysis/analysis.py
@@ -1,5 +1,5 @@
-# This is a sample Python script.
 import re
+import os
 
 
 class Analysis:
@@ -8,23 +8,44 @@ class Analysis:
     """
 
     def __init__(self, html):
-        # self.path = path
         self.html = html
-        # self.data_path = data_path
         self.scripts = []
-        self.get_tags()
+        self.read_directory()
 
-    def get_tags(self):
+    def read_directory(self):
         """
-        Identify & strip tags from collection object
+                Read all response text files in the given directory
+                :return:
+                """
+        os.chdir(self.html)
+        for file in os.listdir():
+            if file.endswith("response.txt"):
+                to_read = f"{os.getcwd()}/{file}"
+                get_tags(self, to_read)
+        for x in self.scripts:
+            print(x)
+
+
+def get_tags(self, file):
+    """
+        Identify & strip tags from response files
         :return:
         """
-        response = open(self.html)
-        text = response.read()
-        text = text.replace('\n', '')
-        text = text.replace('\t', '')
-        scripts = re.findall('(<script.+?</script>)', text.strip())
-        if scripts:
-            self.scripts = scripts
-            for x in self.scripts:
-                print(x)
+    print("Reading File: " + file)
+    response = open(file)
+    text = response.read()
+    text = text.replace('\n', '')
+    text = text.replace('\t', '')
+    scripts = re.findall('(<script.+?</script>)', text.strip())
+    if scripts:
+        for x in scripts:
+            self.scripts.append(x)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    """
+            Run analysis on all files in the sample data directory
+            :return:
+            """
+    Analysis('../data/samples/dev.unshielded.red')
