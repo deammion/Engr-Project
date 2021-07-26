0ce038edd702bf89789f1e73205b69ce5ae7f18a
alias <txmm@protonmail.com>
2021-07-27 10:31:16 +1200

    Test Package & Stubs

--- plugin/test/operational_test.py
+++ plugin/test/operational_test.py
@@ -1,8 +1,10 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
+
+class OperationTest(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
+
 
 if __name__ == '__main__':
     unittest.main()
