b43639a9dc06aa66220a079b5569533428471ebe
James Houlihan <houlihjame@ecs.vuw.ac.nz>
2021-07-27 12:46:40 +1200

    Revert "Merge branch 'Creating_correct_directory' into 'master'"
    This reverts merge request !17

--- plugin/utilities/util.py
+++
@@ -1,8 +0,0 @@
-def print_header(header: str):
-    sz = 20 - int(len(header)/2)
-    print("=" * sz, header, "=" * sz)
-
-
-def print_sub(header: str):
-    sz = 20 - int(len(header)/2)
-    print(" " * sz, header, " " * sz)
