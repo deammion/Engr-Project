0b35231841ed7195bbb4b27ac7129f460ea83da2
timothy <txmm@protonmail.com>
2021-08-31 10:26:21 +1200

    Re-upload setup script

---
+++ plugin/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='plugin',
+    version='3.7',
+    packages=['analysis', 'utilities', 'collection', 'operational'],
+    description='Import dependencies'
+)
