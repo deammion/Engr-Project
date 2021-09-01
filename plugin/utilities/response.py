d3c1a0610e00a74af9d6a95b36c55f7ff69fe086
Nathan <nathanwong991@gmail.com>
2021-09-02 11:50:52 +1200

    Fixing pylint errors
    Added comments and fixed the errors.
    Co-Authored-By: Jaya Narayan <narayajaya@gitlab.com>
    Co-Authored-By: Nathan Wong  <wongnath@gitlab.com>

--- plugin/utilities/response.py
+++ plugin/utilities/response.py
@@ -1,21 +1,41 @@
+"""
+Class which stores and processes the response information
+"""
+
+from __future__ import absolute_import
 import time
 
-"""
-.....
-"""
 
-class response:
-
+class Response:
+    """
+    Response object
+    """
 
     def __init__(self, flow):
+        """
+        Initialise the response information
+        """
         self.flow = flow
         self.time = str(time.time())
 
     def get_time(self):
+        """
+        Get the time fo the response
+        :return: the time of the response as a string
+        """
         return self.time
 
     def get_response(self):
+        """
+        Get the response flow
+        :return: The response flow
+        """
         return self.flow
 
     def get_response_content(self):
+        """
+        Get the response content
+        :return: The response content
+        """
         return self.flow.response.text
+    
