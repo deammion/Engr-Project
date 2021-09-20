e56777e22c0e92bf65b9d4ffddbd56b52c26c3de
Isabella <isabellaketley@gmail.com>
2021-09-20 21:37:05 +1200

    Added code to load a flow from a file
    Added code to load file so tests can start being written

--- plugin/test/test_operational.py
+++ plugin/test/test_operational.py
@@ -2,11 +2,26 @@
 Test Operation Phase
 """
 
+from __future__ import absolute_import
+import os
 
-def test_match():
-    """
-    Placeholder method
-    :return: -
-    """
+from operational.operational import Operational
+from utilities.util import root_dir
 
-    assert True
+from mitmproxy import io, http
+from mitmproxy.exceptions import FlowReadException
+
+
+def load_flow(filename):
+    """
+    Create a method to load a flow so the operation class can be created
+    """
+    with open(filename, "rb") as logfile:
+        freader = io.FlowReader(logfile)
+        try:
+            for f in freader.stream():
+                if isinstance(f, http.HTTPFlow):
+                    return f
+        except FlowReadException as e:
+            print(f"Flow file corrupted: {e}")
+
