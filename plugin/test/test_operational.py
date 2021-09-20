62a17fe3d8a1a99d2a346f232bbc3ec1166786af
Isabella <isabellaketley@gmail.com>
2021-09-20 21:44:35 +1200

    Created first test for operational to check nonce tags
    Co-Authored-By: Isabella Tomaz Ketley <tomazisab@gitlab.com>
    Co-Authored-By: Nathan Wong <wongnath@gitlab.com>

--- plugin/test/test_operational.py
+++ plugin/test/test_operational.py
@@ -25,3 +25,29 @@ def load_flow(filename):
         except FlowReadException as e:
             print(f"Flow file corrupted: {e}")
 
+
+def test_match():
+    """
+    Create a test to check that nonce tags are correctly added to all safe script tags
+    """
+    flow = load_flow(root_dir() + '\\flowInfo.txt')
+    operational = Operational(flow)
+    operational._nonce = "THIS_IS_NONCE"
+
+    # write the created output to a file for later comparison
+    f = open(root_dir() + '/data/outputs/actual/operational.txt', "w")
+    f.write(operational.add_nonce_to_html())
+    f.close()
+
+    # read both the expected and actual files to compare them
+    expected = open(os.path.join(root_dir(), 'data/outputs/expected/operational.txt')).read()
+    actual = open(os.path.join(root_dir(), 'data/outputs/actual/operational.txt')).read()
+
+    if expected == actual:
+        assert True
+    else:
+        assert False
+
+
+test_match()
+
